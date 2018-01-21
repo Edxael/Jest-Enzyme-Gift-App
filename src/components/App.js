c5f07fe8156f6734541ac66a83b52efa6131440a
Edxael <zerphyr.r@gmail.com>
2018-01-21 00:01:49 -0700

    pass reders correctly test

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>Hello React.js Testing</div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+
+Enzyme.configure({ adapter: new Adapter() })
+const app = Enzyme.shallow(<App />)
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot()
+})
