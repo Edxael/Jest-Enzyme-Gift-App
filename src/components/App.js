d474a025b0e68a7269bfa048b3b304efbd56fe6a
Edxael <zerphyr.r@gmail.com>
2018-01-25 22:29:49 -0700

    add new teste to react.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = { gifts: [] }
+  }
+
   render() {
     return(
-      <div>Hello React.js Testing</div>
+      <div>
+        <h2>Gift Giver</h2>
+      </div>
     )
   }
 }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -10,3 +10,11 @@ const app = Enzyme.shallow(<App />)
 it('renders correctly', () => {
   expect(app).toMatchSnapshot()
 })
+
+it('initializes the `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([])
+})
+
+it('adds  a new gift to `state` when clicking the `add gift button`', () => {
+  expect()
+})
